850060a42d83e1c210001799d9f528b4dd6bee81
AffaganoIsTheWay <147073436+AffaganoIsTheWay@users.noreply.github.com>
2024-10-21 16:11:44 +0200

    fix bandwidth

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
-#include <chrono>    // Time counter
+#include <ctime>
+#include <cstdlib>
 
-using namespace std::chrono;
-
-#define N 1000
+#define N 100
 //Define row and colums separate
 //#define C1
 //#define R1
 //#define C2
 //#define R2
 
-float** matrixMultuply(float mat1[][N], float mat2[][N]){
+float** matrixMultuply(float** mat1, float** mat2){
 
-    float** result = 0;
-    result = new float*[N];
+    float** result = static_cast<float**>(malloc(N * sizeof(float)));
 
     for (int i = 0; i < N; i++) {
-        result[i] = new float[N];
+        result[i] = static_cast<float*>(malloc(N * sizeof(float)));
         for (int j = 0; j < N; j++) {
             result[i][j] = 0;
 
@@ -30,8 +28,13 @@ float** matrixMultuply(float mat1[][N], float mat2[][N]){
 }
 
 int main(){
-    float mat1[N][N], mat2[N][N];
+
+    float** mat1 = static_cast<float**>(malloc(N * sizeof(float)));
+    float** mat2 = static_cast<float**>(malloc(N * sizeof(float)));
+
     for(int i = 0; i < N; i++){
+        mat1[i] = static_cast<float*>(malloc(N * sizeof(float)));
+        mat2[i] = static_cast<float*>(malloc(N * sizeof(float)));
         for(int j = 0; j < N; j++){
             mat1[i][j] = i + j;
             mat2[i][j] = i - j;
@@ -40,21 +43,27 @@ int main(){
 
     //check if the multiplication is duable
 
-    auto beg = high_resolution_clock::now();
-
+    clock_t start = clock();
+    
     float** res = matrixMultuply(mat1, mat2);
 
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(end - beg);
+    clock_t end = clock();
+    double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;  // Time in seconds
 
     // Displaying the elapsed time
-    std::cout << "Elapsed Time: " << duration.count() << " milliseconds" << std::endl;
+    std::cout << "Elapsed Time: " << duration << " seconds" << std::endl;
 
     //bandwidth AND flops
     double data_transfer = 3.0 * ((N * N * N) * sizeof(int));
-    double bandwidth = data_transfer / (duration.count() * 1e6);
-    double flops = (N*N*N) / (duration.count() * 1e-3);
+    double bandwidth = data_transfer / (duration * 1e8);
+    double flops = (N*N*N) / duration;
 
     std::cout << "Effective bandwith: " << bandwidth << " GB/s" << std::endl;
     std::cout << "FLOPS: " << flops << std::endl;
+
+    free(mat1);
+    free(mat2);
+    free(res);
+
+    return 0;
 }
